49326cd4ebdf079635e8f662a561fcc9ce599cf3
milimcpt025 <milimcpt025@student.wethinkcode.co.za>
2025-11-03 14:58:37 +0200

    game of life with pygame

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 import os
 from time import sleep
 from random import choice
-
-def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
-
-def colour_board(board):
-    ENDC = '\033[m'
-    for row in board:
-        list1 = []
-        for cell in row:
-            if cell == 'd':
-                colour =  f'\033[100m'
-            elif cell == 'l':
-                    colour =  f'\033[107m'
-            list1.extend((colour + f" ", ENDC))
-        print(*list1)
+import pygame
+from sys import exit
+from time import sleep
 
 def get_valid_cells(board, y_axis, x_axis):
     valid_cells = []
@@ -79,24 +67,79 @@ def make_board(grid, density):
     return board
 
 
+def input_cells(board, pixel_size, grid):
+    cells = board
+    pygame.init()
+    screen = pygame.display.set_mode((
+        int(grid.split('x')[0]) * pixel_size,
+        int(grid.split('x')[1]) * pixel_size
+    ))
+    clock = pygame.time.Clock()
+    running = True
+    try:
+        while running:
+            screen.fill((0, 0, 0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            for j, cell in enumerate(cells):
+                for i, row in enumerate(cell):
+                    box = pygame.Surface((pixel_size, pixel_size))
+                    box_rect = box.get_rect(topleft=(pixel_size * i, pixel_size * j))
+                    if row == 'l':
+                        box.fill("White")
+                    else:
+                        box.fill("Black")
+                    screen.blit(box, box_rect)
+                    mouse_pos = pygame.mouse.get_pos()
+                    screen.blit(box, (pixel_size * i, pixel_size * j))
+                    if box_rect.collidepoint(mouse_pos) and pygame.mouse.get_pressed()[0]:
+                        cells[int(mouse_pos[1] / pixel_size)][int(mouse_pos[0] / pixel_size)] = ('l', 'd')[cells[int(mouse_pos[1] / pixel_size)][int(mouse_pos[0] / pixel_size)] == 'l']
+                        sleep(0.1)
+            pygame.display.update()
+            clock.tick(60)
+    except:
+        return cells
+
 def main():
+    pixel_size = 5
     grid = '150x150'
     density = 2
     auto = True
-    delay = 0.1
-    grid, density, auto, delay = settings(grid, density, auto, delay)
-    print(grid, density, auto, delay)
+    delay = 0
+    grid, density, auto, delay, pixel_size = settings(grid, density, auto, delay, pixel_size)
+    print(grid, density, auto, delay, pixel_size)
     board = make_board(grid, density)
+    board = input_cells(board, pixel_size, grid)
+    screen = pygame.display.set_mode((
+        int(grid.split('x')[0]) * pixel_size,
+        int(grid.split('x')[1]) * pixel_size
+                                      ))
+    pygame.display.set_caption('Game of Life')
+    clock = pygame.time.Clock()
     while True:
-        clear()
-        colour_board(board)
+        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+        cell = pygame.Surface((pixel_size, pixel_size))
+        for j, row in enumerate(board):
+            for i, status in enumerate(row):
+                if status == 'l':
+                    cell.fill('White')
+                else:
+                    cell.fill('Black')
+                screen.blit(cell, (i * pixel_size, j * pixel_size))
+        pygame.display.update()
+        clock.tick(60)
         if not auto:
             input('Press enter to continue\n')
         else:
             sleep(delay)
         change_board(board)
 
-def settings(grid, density, auto, delay):
+def settings(grid, density, auto, delay, pixel_size):
     while True:
         user_input = input('>> ').lower().strip()
         if user_input.startswith('help'):
@@ -105,6 +148,7 @@ def settings(grid, density, auto, delay):
     setdensity (0-10) ------  set the density of live cells on the board. 0 is 0% live cells ,5 is 50%, 10 is 100% (example: setdensity 4)
     setauto (True/False) -----  choose whether board goes to the next iteration automatically (example: setauto True)                  
     setdelay (0-60) -----  set time in seconds between each iteration when on auto (example: setdelay 0.2)
+    setpixel (1-100) ----- set pixel size. 10 is recommended if you plan to manually input cells. (example: setpixel 5)
     return ----- exit settings                       
     """)
         if user_input.startswith('setgrid '):
@@ -117,8 +161,11 @@ def settings(grid, density, auto, delay):
             delay = float(user_input.replace('setdelay ', ''))
         if user_input.startswith('return'):
             break
+        if user_input.startswith('setpixel'):
+            pixel_size = int(user_input.replace('setpixel ', ''))
 
-    return grid, density, auto, delay
+
+    return grid, density, auto, delay, pixel_size
 
 main()
 
